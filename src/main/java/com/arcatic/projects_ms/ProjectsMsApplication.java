7e421f220cdf414482e84934f793a1f6dae75237
didieracm <82002809+didieracm@users.noreply.github.com>
2021-12-09 12:20:55 -0500

    Initial commit

---
+++ src/main/java/com/arcatic/projects_ms/ProjectsMsApplication.java
@@ -0,0 +1,13 @@
+package com.arcatic.projects_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsMsApplication.class, args);
+	}
+
+}
